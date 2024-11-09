260f04c308ee181ee28b175d5d1aadd8611cd2c4
yongu2000 <yongu2000@naver.com>
2024-11-10 01:57:13 +0900

    feat: 주문 입력 시 잘못된 입력 검증 에러메세지 추가

--- src/main/java/store/domain/product/ProductDto.java
+++ src/main/java/store/domain/product/ProductDto.java
@@ -32,7 +32,7 @@ public record ProductDto(
     private static void validateInputString(String inputString) {
         String[] stringProducts =  Stream.of(inputString.split(",")).map(String::trim).toArray(String[]::new);
         Arrays.stream(stringProducts).forEach(stringProduct -> {
-            if (!ITEM_PATTERN.matcher(stringProduct).matches()) throw new IllegalArgumentException();
+            if (!ITEM_PATTERN.matcher(stringProduct).matches()) throw new IllegalArgumentException("[ERROR] 올바르지 않은 형식으로 입력했습니다. 다시 입력해 주세요.");
         });
     }
 
--- src/test/java/store/domain/product/ProductDtoTest.java
+++ src/test/java/store/domain/product/ProductDtoTest.java
@@ -9,4 +9,21 @@ import static org.assertj.core.api.Assertions.*;
 
 class ProductDtoTest {
 
+    @DisplayName("문자열로부터 ProductDto 생성")
+    @Test
+    void 문자열로부터_Product_Dto_생성() {
+        List<ProductDto> from = ProductDto.from("[콜라-2], [사이다-3]");
+
+        assertThat(from).contains(ProductDto.of("콜라", 2), ProductDto.of("사이다", 3));
+    }
+
+    @DisplayName("잘못된 문자열 입력시 에러 출력")
+    @Test
+    void 잘못된_문자열_입력시_에러() {
+
+        assertThatThrownBy(() -> ProductDto.from("[콜라-2], 사이다-3]"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR] 존재하지 않는 상품입니다. 다시 입력해 주세요.");
+    }
+
 }

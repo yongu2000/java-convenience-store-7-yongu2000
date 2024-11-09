f29673f03b3cb98a1f2d0ad153e6b3a9e742caa1
yongu2000 <yongu2000@naver.com>
2024-11-10 03:40:05 +0900

    refactor: OutputView 출력포맷 변경

--- src/main/java/store/view/OutputView.java
+++ src/main/java/store/view/OutputView.java
@@ -8,16 +8,16 @@ public class OutputView {
     private static final String STORE_PRODUCTS = "현재 보유하고 있는 상품입니다.\n";
 
     private static final String RECEIPT_START = "==============W 편의점================\n";
-    private static final String RECEIPT_PRODUCTS_START = "상품명\t\t수량\t금액\n";
-    private static final String RECEIPT_PRODUCT = "%s\t\t%d\t%,d\n";
-    private static final String RECEIPT_PROMOTION_PRODUCTS_START = "=============증\t정===============\n";
-    private static final String RECEIPT_PROMOTION_PRODUCT = "%s\t\t%d\n";
+    private static final String RECEIPT_PRODUCTS_START = "상품명\t\t\t수량\t\t\t금액\n";
+    private static final String RECEIPT_PRODUCT = "%s\t\t\t%d\t\t\t%,d\n";
+    private static final String RECEIPT_PROMOTION_PRODUCTS_START = "=============증\t\t정===============\n";
+    private static final String RECEIPT_PROMOTION_PRODUCT = "%s\t\t\t%d\n";
 
     private static final String RECEIPT_END = "====================================\n";
-    private static final String RECEIPT_TOTAL_PRICE = "총구매액\t\t%d\t%,d\n";
-    private static final String RECEIPT_PROMOTION_DISCOUNT = "행사할인\t\t\t-%,d\n";
-    private static final String RECEIPT_MEMBERSHIP_DISCOUNT = "멤버십할인\t\t\t-%,d\n";
-    private static final String RECEIPT_CUSTOMER_PRICE = "내실돈\t\t\t%,d\n";
+    private static final String RECEIPT_TOTAL_PRICE = "총구매액\t\t\t%d\t\t\t%,d\n";
+    private static final String RECEIPT_PROMOTION_DISCOUNT = "행사할인\t\t\t\t\t\t-%,d\n";
+    private static final String RECEIPT_MEMBERSHIP_DISCOUNT = "멤버십할인\t\t\t\t\t\t-%,d\n";
+    private static final String RECEIPT_CUSTOMER_PRICE = "내실돈\t\t\t\t\t\t%,d\n";
 
     public void printStoreInformation(String products) {
         System.out.print(GREETING);
